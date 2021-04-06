384b5ed4d7eee036af1c09591af7bcce6ac1909e
Emircan Duman <mail@eduman.de>
2021-04-06 10:45:31 +0200

    initial setup

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors','On')
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Web Anwendung</title>
+</head>
+<body>
+<header class="jumbotron">
+    <div class="container">
+        <h1>Willkommen</h1>
+    </div>
+</header>
+<script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
